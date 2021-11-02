aef73c053f3fad3f00873b1a107742c4099ead6a
Dusty Paper Box <sora1211x@gamil.com>
2021-11-02 13:23:07 +0000

    Add mongoose and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import Router from 'koa-router';
 import api from './api';
 
+dotenv.config();
 const app = new Koa();
 const router = new Router();
+
+const { PORT, MONGO_URI } = process.env;
+
+if (typeof MONGO_URI === 'string') {
+  mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+}
 
 router.use('/api', api.routes());
 
 app.use(bodyParser()).use(router.routes()).use(router.allowedMethods());
 
-app.listen(5001, () => {
-  console.log('Listen to port 5001');
+const port = PORT || 5001;
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
 });
