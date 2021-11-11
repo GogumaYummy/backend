007bee1aea6b6df7039e620d645e41a30b0225c2
Dusty Paper Box <sora1211x@gamil.com>
2021-11-11 10:26:38 +0000

    Add login

--- src/api/auth/auth.ctrl.ts
+++ src/api/auth/auth.ctrl.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 import { IMiddleware } from 'koa-router';
-import User, { IUser } from '../../models/user';
+import User from '../../models/user';
 
 export const register: IMiddleware = async (ctx) => {
   const schema = Joi.object().keys({
@@ -35,6 +35,38 @@ export const register: IMiddleware = async (ctx) => {
     }
   }
 };
-export const login: IMiddleware = async (ctx) => {};
+export const login: IMiddleware = async (ctx) => {
+  const { username, password } = ctx.request.body;
+
+  const schema = Joi.object().keys({
+    username: Joi.string().required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+
+    const valid = await user.checkPassword(password);
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+  } catch (e) {
+    if (e instanceof Error) {
+      ctx.throw(500, e);
+    }
+  }
+};
 export const check: IMiddleware = async (ctx) => {};
 export const logout: IMiddleware = async (ctx) => {};
